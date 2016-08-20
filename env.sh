9abe41ceb3966337cc16cf1ad57f868f4dd46876
Vicent Marti <tanoku@gmail.com>
2016-08-20 14:40:46 +0200

    Some aliases!

--- env.sh
+++ env.sh
@@ -1,29 +1,42 @@
 export TMP=/tmp
-export NVIM_TUI_ENABLE_CURSOR_SHAPE=1
 export TERM=xterm-256color
+export PATH="$HOME/.dotfiles/bin:$PATH"
 
 alias ls='ls --color=auto -F'
 alias make='make -j4'
 alias gvim='gvim --sync'
 
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
 PREFIX="${PREFIX:-/usr/local}"
 
-if [[ -d $PREFIX/share/chruby/ ]]; then
+if [ -d $PREFIX/share/chruby/ ]; then
 	source $PREFIX/share/chruby/chruby.sh
 	source $PREFIX/share/chruby/auto.sh
 	chruby 2.3.1
 fi
 
-if [[ -d $HOME/.nodenv/ ]]; then
+if [ -d $HOME/.nodenv/ ]; then
 	export PATH="$HOME/.nodenv/bin:$PATH"
 	eval "$(nodenv init -)"
 fi
 
-if [[ -d $PREFIX/go/ ]]; then
+if [ -d $PREFIX/go/ ]; then
     export GOPATH="$HOME/src/gopath"
     export PATH="$GOPATH/bin:$PREFIX/go/bin:$PATH"
 fi
 
 if [ -n "$DISPLAY" ]; then
     xset -b
+fi
+
+if command_exists most; then
+    export MANPAGER="$(which most)"
+    alias less='most'
+fi
+
+if command_exists exa; then
+    alias ls='exa -l'
 fi
